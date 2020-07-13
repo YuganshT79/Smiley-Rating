dae1cb9a6a8ac627d9034bccf8ccc8bd29c1e1d3
YuganshT79 <yugansh39@gmail.com>
2020-07-14 01:48:38 +0530

    Modify SmileyState to get state objects

--- smileyrating/src/main/java/dev/yuganshtyagi/smileyrating/SmileyState.kt
+++ smileyrating/src/main/java/dev/yuganshtyagi/smileyrating/SmileyState.kt
@@ -3,10 +3,23 @@ package dev.yuganshtyagi.smileyrating
 /**
  *  Created by Yugansh on 7/12/2020
  */
-sealed class SmileyState(val value: Int) {
-    object Sad : SmileyState(0)
-    object Neutral : SmileyState(1)
-    object Okay : SmileyState(2)
-    object Happy : SmileyState(3)
-    object Amazing : SmileyState(4)
+sealed class SmileyState {
+    object Sad : SmileyState()
+    object Neutral : SmileyState()
+    object Okay : SmileyState()
+    object Happy : SmileyState()
+    object Amazing : SmileyState()
+
+    companion object {
+        fun of(value: Int): SmileyState {
+            return when (value) {
+                0 -> Sad
+                1 -> Neutral
+                2 -> Okay
+                3 -> Happy
+                4 -> Amazing
+                else -> Neutral
+            }
+        }
+    }
 }
